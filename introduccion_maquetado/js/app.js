1fb015553976d375ee2514941912cfe0356ec0a0
furenku <furenku@gmail.com>
2017-01-25 21:50:11 -0600

    ejercicio maquetado

--- introduccion_maquetado/js/app.js
+++ introduccion_maquetado/js/app.js
@@ -16,8 +16,13 @@ $(document).ready(function(){
 
       squareH()
 
+      verticalCenter('.caja')
+      verticalCenter('.v-center')
+
    })
+
    // obligar a la ventana a ejecutar acción de resize
+
    $(window).trigger('resize')
 
    console.log("introducción maquetado lista")
@@ -25,6 +30,57 @@ $(document).ready(function(){
 
 })
 
+function verticalCenter( selector ) {
+
+   // Seleccionar Elementos a Centrar
+
+   $( selector ).each(function(){
+
+      // Seleccionar Cada uno
+      elemento = $(this)
+
+      // Conocer Altura de Contenedor
+
+      altura_contenedor = elemento.height()
+
+      // Conocer Altura Total de Elementos Hijos:
+
+      // seleccionar hijos
+
+      hijos = elemento.children()
+
+      // empezamos a contar desde 0
+
+      altura_elementos = 0
+
+      // iterar por todos los hijos
+
+      hijos.each(function(){
+
+         // almacenar hijo en envariable para claridad
+
+         hijo = $(this)
+
+         // aumentamos altura total hijos
+
+         altura_elementos += hijo.height()
+
+      })
+
+
+      // Calcular diferencia entre alturas
+
+      diferencia = altura_contenedor - altura_elementos
+
+      // Aplicar mitad de diferencia arriba de Elementos
+
+      elemento.css({
+         paddingTop: diferencia / 2
+      })
+
+   })
+
+}
 
 function squareH() {
 
