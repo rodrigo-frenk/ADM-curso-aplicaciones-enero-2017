c7cc333b2585c9b62c199d908b0f62e1e2c6351c
rodrigo-frenk <rodrigo.frenk@gmail.com>
2017-03-08 21:58:21 -0600

    implementando conversaciones. notificaciones en tiempo real de cambios de participantes

--- introduccion_nodejs/index.js
+++ introduccion_nodejs/index.js
@@ -17,7 +17,7 @@ var clients = []
 
 var conversations = []
 
-var groupChatId = 0
+var groupChatId = '#general'
 
 /*
 
@@ -45,7 +45,7 @@ http.listen(3000, function(){
 
 
 // io es un gestor de conexiones:
-// declararemos funcion "callback" a ejecutarse al conectarse un cliente
+// declararemos funciones "callback" a ejecutarse al conectarse un cliente
 
 /*
 // cada mensaje enviado por un usuario está
@@ -60,7 +60,7 @@ http.listen(3000, function(){
 */
 
 
-startGroupConversation()
+startGeneralConversation()
 
 
 io.on('connection', function(socket){
@@ -74,7 +74,7 @@ io.on('connection', function(socket){
       username: "Sin Nombre",
       loginComplete: false,
       lastLogin: new Date(),
-      conversations: []
+      // conversations: []
    }
 
    userListChanged()
@@ -83,7 +83,6 @@ io.on('connection', function(socket){
 
    socket.on('disconnect', function(){
       userLogout(socket)
-      userListChanged()
    });
 
    socket.on('set username', function(payload){
@@ -92,7 +91,6 @@ io.on('connection', function(socket){
       clients[socket.id].loginComplete = true
 
       userLogin(socket)
-      userListChanged()
 
    });
 
@@ -113,25 +111,25 @@ io.on('connection', function(socket){
          message: payload.message
       }
 
-      // Verificar si el emisor especificó un destino:
-      // esto lo haremos revisando si la variable tiene un valor asignado
-      if(typeof( payload.targetId ) === "undefined") {
-
-         // si no, mandó un mensaje grupal
-
-         console.log( "TYPEOF", typeof( payload.targetId ) )
-         //
-         io.emit('chat message', messagePayload )
-
-      } else {
-
-         //mesnaje privado
-         // almacenar id del destinatario en el mensaje
-         messagePayload.targetId = payload.targetId
-
-         clients[ payload.targetId ].socket.emit('chat message', messagePayload )
-
-      }
+      // // Verificar si el emisor especificó un destino:
+      // // esto lo haremos revisando si la variable tiene un valor asignado
+      // if(typeof( payload.targetId ) === "undefined") {
+      //
+      //    // si no, mandó un mensaje grupal
+      //
+      //    console.log( "TYPEOF", typeof( payload.targetId ) )
+      //    //
+      //    io.emit('chat message', messagePayload )
+      //
+      // } else {
+      //
+      //    //mesnaje privado
+      //    // almacenar id del destinatario en el mensaje
+      //    messagePayload.targetId = payload.targetId
+      //
+      //    clients[ payload.targetId ].socket.emit('chat message', messagePayload )
+      //
+      // }
 
 
 
@@ -161,10 +159,14 @@ function userListChanged() {
 function userLogin(socket) {
 
    addToConversation( groupChatId, socket.id )
+   addToConversation( 'abcdefg', socket.id )
 
-   console.log( "conversation", conversations[groupChatId].participants )
+
+   // console.log( "conversation", conversations[groupChatId].participants )
 
    io.emit('user login', getUserPayload(socket))
+
+   userListChanged()
 
 }
 
@@ -173,17 +175,19 @@ function userLogout(socket) {
 
    // eliminar usuario de todas las conversaciones en las que esté
 
-   oldConversations = clients[socket.id].conversations
+   userConversations = getUserConversations( socket.id )
 
-   for( i in oldConversations ) {
+   for( i in userConversations ) {
       removeFromConversation( i, socket.id )
    }
 
-   console.log( "conversation", conversations[groupChatId].participants )
+   // console.log( "conversation", conversations[groupChatId].participants )
 
    io.emit('user logout', getUserPayload(socket))
 
    delete clients[socket.id]
+
+   userListChanged()
 
 }
 
@@ -232,53 +236,94 @@ function getActiveUsers() {
 }
 
 
-function startGroupConversation() {
+function startGeneralConversation() {
 
    var newConversation = {
+      name: "General",
       participants: [],
-      messages: [],
+      messages: [
+         {
+            username: "Nombre de Usuario",
+            message: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quibusdam dolore laboriosam quis.",
+            date: new Date(2017, 3, 8, 18, 3)
+         },
+         {
+            username: "Otro Usuario",
+            message: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quibusdam dolore laboriosam quis.",
+            date: new Date(2017, 3, 8, 19, 10)
+         },
+
+      ],
       startedAt: new Date()
    }
 
    conversations[ groupChatId ] = newConversation
+
+
+   conversations[ 'abcdefg' ] = {
+      name: "Ejemplo Dos",
+      participants: [],
+      messages: [
+         {
+            username: "Tercer Usuario",
+            message: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quibusdam dolore laboriosam quis.",
+            date: new Date(2017, 3, 8, 20, 20)
+         },
+         {
+            username: "Usuario Cuarto",
+            message: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quibusdam dolore laboriosam quis.",
+            date: new Date(2017, 3, 8, 21, 21)
+         },
+      ],
+      startedAt: new Date()
+   }
 
 }
 
 
 function addToConversation( conversationId, userId ) {
 
-   chosenConversation = conversations[ conversationId ]
+   var chosenConversation = conversations[ conversationId ]
 
-   // buscar el usuario en la conversación
+   // buscar el usuario en la conversación,
+   // obtener el índice dentro del arreglo
    chosenUserIndex = chosenConversation.participants.indexOf( userId )
 
    // si el usuario no está en la conversación, añádelo
    if( chosenUserIndex === -1 ) {
+
       chosenConversation.participants.push( userId )
 
-      clients[ userId ].conversations.push( conversationId )
+      // clients[ userId ].conversations.push( conversationId )
    }
 
+
+   notifyConversationPeers( getUserConversations( userId ) )
+
 }
+
+
 
 function removeFromConversation( conversationId, userId ) {
 
 
    chosenConversation = conversations[ conversationId ]
 
+   userConversations = getUserConversations( userId )
+
    // buscar el usuario en la conversación
    chosenUserIndex = chosenConversation.participants.indexOf( userId )
 
-   // si el usuario no está en la conversación, añádelo
+   // si el usuario sí está en la conversación, bórralo de ésta
    if( chosenUserIndex !== -1 ) {
       // sacar elemento del arreglo usando splice
       chosenConversation.participants.splice( chosenUserIndex, 1 )
 
-      oldConversationIndex = clients[userId].conversations.indexOf( conversationId )
-
-      if( oldConversationIndex !== -1 ) {
-         clients[userId].conversations.splice( oldConversationIndex, 1 )
-      }
+      // oldConversationIndex = clients[userId].conversations.indexOf( conversationId )
+      //
+      // if( oldConversationIndex !== -1 ) {
+      //    clients[userId].conversations.splice( oldConversationIndex, 1 )
+      // }
 
       // eliminar conversaciones vacías, exceptuando la grupal:
       if(
@@ -287,6 +332,57 @@ function removeFromConversation( conversationId, userId ) {
          conversations[conversationId].participants.length <= 0
       ) {
          delete conversations[conversationId]
+      }
+
+   }
+
+   notifyConversationPeers( userConversations )
+
+
+}
+
+
+function getUserConversations( id ) {
+
+   var userConversations = {}
+
+   for( i in conversations ) {
+
+      var index = conversations[i].participants.indexOf( id )
+
+      if( index !== -1 ) {
+
+         userConversations[i] = conversations[ i ]
+
+      }
+
+   }
+
+   return userConversations
+
+}
+
+
+function updateUserConversations( userId ) {
+
+   var payload = {
+      conversations: getUserConversations( userId )
+   }
+
+   clients[ userId ].socket.emit('update conversations', payload )
+
+}
+
+
+
+function notifyConversationPeers( userConversations ) {
+
+   for( i in userConversations ) {
+
+      otherParticipants = userConversations[i].participants
+
+      for( j in otherParticipants ) {
+         updateUserConversations( otherParticipants[j] )
       }
 
    }
---
+++ introduccion_nodejs/todo.md
@@ -0,0 +1,7 @@
+to-do
+
+una conversacion se cierra cuando solo queda 1
+
+
+
+- mantener registro de cuándo viste los últimos mensajes de una conversacion
